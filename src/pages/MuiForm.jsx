8943ec74441e60ad73ef712cfe4579228cf3f16a
Waqqars987 <waqqars987@gmail.com>
2023-09-30 12:33:15 +0530

    resolved eslint warnings

--- src/pages/MuiForm.jsx
+++ src/pages/MuiForm.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import { yupResolver } from '@hookform/resolvers/yup';
-import { Button, FormControl, FormHelperText, FormLabel, Stack, TextField } from '@mui/material';
-import { DatePicker, DateCalendar } from '@mui/x-date-pickers';
+import { Button, FormControl, FormHelperText, /*FormLabel,*/ Stack, TextField } from '@mui/material';
+import { DatePicker /*DateCalendar*/ } from '@mui/x-date-pickers';
 import { useController, useForm, Controller } from 'react-hook-form';
 import * as yup from 'yup';
 
