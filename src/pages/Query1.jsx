03a0646e9c56f678b5d3250c1b183d168f701e01
Waqqars987 <waqqars987@gmail.com>
2024-10-04 11:53:26 +0530

    Update query 1

--- src/pages/Query1.jsx
+++ src/pages/Query1.jsx
@@ -1,16 +1,32 @@
+import { useState } from 'react';
 import { useQuery } from '@tanstack/react-query';
+import { Button } from '@mui/material';
 import axios from 'axios';
 
 export default function Query1() {
+	const [id, setId] = useState(0);
 	const { data, isLoading, isPending, isFetching } = useQuery({
-		queryKey: ['posts'],
-		queryFn: getPosts
+		queryKey: ['posts', id],
+		queryFn: getPosts,
+		enabled: id > 0
 	});
 	console.log({ data, isLoading, isPending, isFetching });
-	return <h1>Query 1</h1>;
+	return (
+		<>
+			<h1>Query 1</h1>
+			<Button
+				variant='outlined'
+				onClick={() => setId(prevId => prevId + 1)}
+				sx={{ alignSelf: 'flex-start' }}
+				size='large'
+			>
+				Increment ID
+			</Button>
+		</>
+	);
 }
 
-export async function getPosts() {
-	const { data } = await axios.get('https://jsonplaceholder.org/posts');
+export async function getPosts({ queryKey }) {
+	const { data } = await axios.get('https://jsonplaceholder.org/posts/' + queryKey[1]);
 	return data;
 }
